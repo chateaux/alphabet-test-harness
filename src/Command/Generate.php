f10bcbcf2e7b87af3e059b98ef4005a283fcbdf2
Brendan Nash <brendan@southeasterdesign.com>
2019-01-26 01:30:25 +0200

    Optimised code.

--- src/Command/Generate.php
+++ src/Command/Generate.php
@@ -19,170 +19,234 @@ class Generate
         $this->zendMathRand = new RNGZendMathRand();
     }
 
+    /**
+     * This organises the various tasks
+     * @param Route $route
+     * @param AdapterInterface $console
+     */
     public function __invoke(Route $route, AdapterInterface $console)
     {
-        $console->clearScreen();
-        $console->writeLine('Make sure your command line is using php 7 +', \Zend\Console\ColorInterface::RED);
+        $loop = true;
 
-        $options = array(
-            '1' => 'Play game',
-            '2' => 'Scaled data',
-            '3' => 'Get random bytes',
-            'q' => 'Quit...'
-        );
-
-        $answer = Prompt\Select::prompt(
-            'Select an option: ',
-            $options,
-            false,
-            false
-        );
-
-        if ($answer == '1') {
+        while ($loop == true) {
             $console->clearScreen();
-            $this->playGame($console);
-        }
+            $console->writeLine('Make sure your command line is using php 7 +', \Zend\Console\ColorInterface::RED);
 
-        if ($answer == '2') {
-            $console->clearScreen();
-            $this->scaleData($console);
-        }
+            $options = array(
+                '1' => 'Play game',
+                '2' => 'Scaled data',
+                '3' => 'Get random bytes',
+                'q' => 'Quit...'
+            );
 
-        if ($answer == '3') {
-            $console->clearScreen();
-            $this->getBytes($console);
-        }
+            $answer = Prompt\Select::prompt(
+                'Select an option: ',
+                $options,
+                false,
+                false
+            );
 
-        if ($answer == 'q') {
-            $console->clearScreen();
-        }
+            if ($answer == '1') {
+                $console->clearScreen();
+                $this->playGame($console);
+            }
 
-        $console->writeLine();
+            if ($answer == '2') {
+                $console->clearScreen();
+                $this->scaleData($console);
+            }
+
+            if ($answer == '3') {
+                $console->clearScreen();
+                $this->getBytes($console);
+            }
+
+            if ($answer == 'q') {
+                $console->clearScreen();
+                $loop = false;
+            }
+            $console->writeLine();
+        }
     }
 
+    /**
+     * Play the actual game on the command line
+     * @param AdapterInterface $console
+     */
     private function playGame(AdapterInterface $console)
     {
-        $prize = [
-            0 => '$0',
-            1 => '$10',
-            2 => '$20',
-            3 => '$30',
-            4 => '$40',
-            5 => '$50',
-            6 => '$60',
-            7 => '$70',
-            8 => '$80',
-        ];
-        $figlet = new \Zend\Text\Figlet\Figlet();
-        echo $figlet->render('Play now!');
-        $console->writeLine("Playing Alphabet. ");
-        $console->writeLine("To win, you must reveal the word 'ALPHABET' by scratching the card.");
-        $console->writeLine("The lucky letter A counts for 2 matches!");
+        $fp = 1;
+        $playCount = 0;
+        $fpIterations = 1000000;
+        $fastplay = Prompt\Confirm::prompt("Fast play (play $fpIterations games a time)? [y/n]");
+        $loop = true;
+        $balance = 0;
+        $ticketCost = 5;
+        $win = 0;
+        $cost = 0;
+        $winLevelStats = [0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0,  8 => 0,];
+        $prize = [0 => 0, 1 => 0, 2 => 0, 3 => 10, 4 => 50, 5 => 1000, 6 => 100000, 7 => 1000000, 8 => 0,];
         $min = 1;
-        $max = 26;
+        $max = 49;
         $selectNumbers = 7;
 
-        $console->writeLine("Selecting $selectNumbers numbers from a pool of $min to $max...");
+        while ($loop == true) {
+            $balance = $balance - $ticketCost;
 
-        $result = $this->zendMathRand->getRandomLetters(
-            $selectNumbers,
-            $min, $max,
-            ['A', 'L', 'P', 'H', 'A', 'B', 'E', 'T'],
-            [
+            if (!$fastplay or $fp > $fpIterations) {
+                $figlet = new \Zend\Text\Figlet\Figlet();
+                echo $figlet->render('Play now!');
+                $console->writeLine("Playing Alphabet, your balance is $$balance, each ticket costs $$ticketCost ");
+                $console->writeLine("To win, you must reveal the word 'ALPHABET' by scratching the card.");
+                $console->writeLine("The lucky letter A counts for 2 matches!");
+                $console->writeLine("Selecting $selectNumbers numbers from a pool of $min to $max...");
+            }
+            $console->write("Count: ".$playCount);
+
+            $result = $this->zendMathRand->getRandomLetters(
+                $selectNumbers,
+                $min, $max,
+                ['A', 'L', 'P', 'H', 'A', 'B', 'E', 'T'],
                 [
-                    'letter' => 'A',
-                    'count' => 2,
-                    'frequency' => 2
+                    [
+                        'letter' => 'A',
+                        'count' => 2,
+                        'frequency' => 2
+                    ]
                 ]
-            ]
-        );
+            );
 
-        $console->writeLine('Scratching in progress: ');
-        for ($i=0;$i<7;$i++) {
-            $console->write('#');
-            usleep(300000);
+            if (!$fastplay) {
+                $console->writeLine('Scratching in progress: ');
+                for ($i = 0; $i < 7; $i++) {
+                    $console->write('#');
+                    usleep(200000);
+                }
+            }
+            $cost = $cost + $ticketCost;
+            $win = $win + $prize[$result['winLevel']];
+            $winLevelStats[$result['winLevel']] = $winLevelStats[$result['winLevel']] + 1;
+            $balance = $balance + $prize[$result['winLevel']];
+
+            if (!$fastplay or $fp > $fpIterations) {
+                $console->writeLine('');
+                $subject = ($result['winLevel'] > 1) ? 'letters' : 'letter';
+                $table = new \Zend\Text\Table\Table([
+                    'columnWidths' => [30, 20, 20]
+                ]);
+                $table->appendRow(['You revealed the letters:', implode(",", $result['letters']), ""]);
+                $table->appendRow(['You matched:', $result['winLevel'] . ' ' . $subject, ""]);
+                $table->appendRow(['You won:', "$" . $prize[$result['winLevel']], ""]);
+                $table->appendRow(['Your balance is:', "$$balance", ""]);
+                $table->appendRow(['Game bets:', "$$cost", ""]);
+                $table->appendRow(['Game collects:', "$$win", ""]);
+                $table->appendRow(['Operator standing:', "$" . ($cost - $win), ""]);
+                foreach ($winLevelStats as $key => $level) {
+                    $table->appendRow(["Level: $key ($$prize[$key])", "$level", "$" . ($level * $prize[$key])]);
+                }
+                echo $table;
+            }
+
+            if (!$fastplay or $fp > $fpIterations) {
+                $fp = 1;
+                $loop = Prompt\Confirm::prompt('Play again? [y/n]');
+                if ($loop) {
+                    $fastplay = Prompt\Confirm::prompt("Fast play (play $fpIterations games a time)? [y/n]");
+                }
+            }
+
+            $fp ++;
+            $playCount ++;
+            $console->clearScreen();
         }
-        $console->writeLine('');
-        $subject =  ($result['winLevel'] > 1) ? 'letters' : 'letter';
-        $table = new \Zend\Text\Table\Table([
-            'columnWidths' => [30, 20]
-        ]);
-        $table->appendRow(['You revealed the letters:', implode(",", $result['letters'])]);
-        $table->appendRow(['You matched:', $result['winLevel'].' '.$subject]);
-        $table->appendRow(['You won:', $prize[$result['winLevel']]]);
-        echo $table;
+
         return;
     }
 
     private function scaleData(AdapterInterface $console)
     {
-        $figlet = new \Zend\Text\Figlet\Figlet();
-        echo $figlet->render('Scaled data!');
-        $console->writeLine("The minimum value is set to 1");
-        $min = 1;
-        $max = Line::prompt(
-            'Enter a max value: (26 - 80): ',
-            false,
-            6
-        );
-        if ($max > 80) {
-            $console->writeLine("Sorry, max number is 80", \Zend\Console\ColorInterface::RED);
-            return;
-        }
-        $generateNumbers = Line::prompt(
-            'How many numbers to generate (10,000 - 10,000,000): ',
-            false,
-            8
-        );
-        if ($generateNumbers > 10000000) {
-            $console->writeLine("Sorry, you can not generate more than 10 million numbers", \Zend\Console\ColorInterface::RED);
-            return;
-        }
-        $iterations = Line::prompt(
-            'How many iterations? (1 - 4) will be stored in files in the /data folder using the format filename-date-file-n: ',
-            false,
-            8
-        );
-        if ($iterations > 4) {
-            $console->writeLine("We only allow up to 4 iterations.", \Zend\Console\ColorInterface::RED);
-            return;
-        }
-        $console->writeLine("Selecting $generateNumbers iterations from the pool ($min / $max)");
-        $console->writeLine('Writing to file, this may take some time!',\Zend\Console\ColorInterface::RED);
-        $filename = $this->config['fileLocation'].$this->config['filenameScale'].\Date('y-m-d-h-i-s').'-'.$generateNumbers.'X('.$min.'to'.$max.')-total('.$iterations.')-file';
-        for($n=1;$n<=$iterations;$n++) {
-            for($i=0;$i<$generateNumbers;$i++) {
-                $result = $this->zendMathRand->getRandomNumber($min, $max);
-                file_put_contents($filename.'-'.$n, $result.PHP_EOL, FILE_APPEND);
+        $loop = true;
+        while ($loop == true) {
+            $figlet = new \Zend\Text\Figlet\Figlet();
+            echo $figlet->render('Scaled data!');
+            $console->writeLine("The minimum value is set to 1");
+            $min = 1;
+            $max = Line::prompt(
+                'Enter a max value: (26 - 80): ',
+                false,
+                6
+            );
+            if ($max > 80) {
+                $console->writeLine("Sorry, max number is 80", \Zend\Console\ColorInterface::RED);
+                return;
             }
-            $console->writeLine("Integers written to file: ".$filename."-".$n);
+            $generateNumbers = Line::prompt(
+                'How many numbers to generate (10,000 - 10,000,000): ',
+                false,
+                8
+            );
+            if ($generateNumbers > 10000000) {
+                $console->writeLine("Sorry, you can not generate more than 10 million numbers", \Zend\Console\ColorInterface::RED);
+                return;
+            }
+            $iterations = Line::prompt(
+                'How many iterations? (1 - 4) will be stored in files in the /data folder using the format filename-date-file-n: ',
+                false,
+                8
+            );
+            if ($iterations > 4) {
+                $console->writeLine("We only allow up to 4 iterations.", \Zend\Console\ColorInterface::RED);
+                return;
+            }
+            $fileSize = ($generateNumbers < 347000) ? "< 1MB" : "~".number_format((float)$generateNumbers/347000, 2, '.', '')."MB";
+            $filename = $this->config['fileLocation'].$this->config['filenameScale'].\Date('YMd-h:i:s').'-'.$generateNumbers.'('.$min.'to'.$max.')-total('.$iterations.')-file';
+            for($n=1;$n<=$iterations;$n++) {
+                $console->writeLine($n." Selecting $generateNumbers iterations from the pool ($min / $max)");
+                $console->writeLine($n." Writing integers to file ($fileSize)",\Zend\Console\ColorInterface::RED);
+                for($i=0;$i<$generateNumbers;$i++) {
+                    $result = $this->zendMathRand->getRandomNumber($min, $max);
+                    file_put_contents($filename.'-'.$n, $result.PHP_EOL, FILE_APPEND);
+                }
+                $console->writeLine("Integers written and stored in file: $filename-$n", \Zend\Console\ColorInterface::BLUE);
+            }
+            $console->writeLine('Ok we are done!',\Zend\Console\ColorInterface::BLUE);
+
+            $loop = Prompt\Confirm::prompt('Run again? [y/n]');
+            $console->clearScreen();
         }
-        $console->writeLine('Ok we are done!',\Zend\Console\ColorInterface::BLUE);
+
         return;
     }
 
     public function getBytes(AdapterInterface $console)
     {
-        $figlet = new \Zend\Text\Figlet\Figlet();
-        echo $figlet->render('Get bytes');
-        $meg = Line::prompt(
-            'Enter total MB to generate (1-20): ',
-            false,
-            8
-        );
-        if ($meg > 20) {
-            $console->writeLine("Sorry, maximum file size is 20MB", \Zend\Console\ColorInterface::RED);
-            return;
+        $loop = true;
+        while ($loop == true) {
+            $figlet = new \Zend\Text\Figlet\Figlet();
+            echo $figlet->render('Get bytes');
+            $meg = Line::prompt(
+                'Enter total MB to generate (1-20): ',
+                false,
+                8
+            );
+            if ($meg > 20) {
+                $console->writeLine("Sorry, maximum file size is 20MB", \Zend\Console\ColorInterface::RED);
+                return;
+            }
+            $console->writeLine("Saving random bytes");
+
+            $result = Rand::getBytes($meg * 1000000);
+
+            $filename = $this->config['fileLocation'] . $this->config['filenameBytes'] . \Date('y-m-d-h-i-s') . '-bytes(' . $meg . 'MB)';
+            $console->writeLine("Random bytes stored to file: " . $filename);
+
+            file_put_contents($filename, $result . PHP_EOL, FILE_APPEND);
+            $console->writeLine('Ok we are done!', \Zend\Console\ColorInterface::BLUE);
+
+            $loop = Prompt\Confirm::prompt('Run again? [y/n]');
+            $console->clearScreen();
         }
-        $console->writeLine("Saving random bytes");
 
-        $result = Rand::getBytes($meg*1000000);
-
-        $filename = $this->config['fileLocation'].$this->config['filenameBytes'].\Date('y-m-d-h-i-s').'-bytes('.$meg.'MB)';
-        $console->writeLine("Random bytes stored to file: ".$filename);
-
-        file_put_contents($filename, $result.PHP_EOL, FILE_APPEND);
-        $console->writeLine('Ok we are done!',\Zend\Console\ColorInterface::BLUE);
         return;
     }
 }
